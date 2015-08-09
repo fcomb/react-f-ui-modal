f4b6d5694a2143c9e5f385b09f394873b5f66c35
Eugene Rodionov <git@theaqua.im>
2015-08-10 01:23:39 +0300

    fix: add link to github in demo

--- example/index.js
+++ example/index.js
@@ -4,7 +4,14 @@ import ExampleModal from 'components/modal';
 import 'styles/default';
 
 React.render((
-  <ExampleModal className="container">
-    <button className="button-cta">Launch example modal</button>
-  </ExampleModal>
+  <section className="container">
+    <div>
+      <h1 className="header-title text-center">
+        <a href="https://github.com/fcomb/react-f-ui-modal" className="header-title-link">Modal component for React</a>
+      </h1>
+      <ExampleModal className="container-modal text-center">
+        <button className="button-cta">Launch example modal</button>
+      </ExampleModal>
+    </div>
+  </section>
 ), document.getElementById('app'));
